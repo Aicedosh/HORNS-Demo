062b42e0a2bbca7923c076a1c0134311195176a0
Andrzej Lamecki <andrzej4172.lama@gmail.com>
2019-11-11 17:48:25 +0100

    Add basic HORNS classes

---
+++ Assets/HORNS/Actions/TestAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAction : BasicAction
+{
+    private bool working = false;
+    private float time = 0f;
+
+    private int count = 0;
+
+    public float Interval;
+
+    //TODO: this name is not the best (change in HORNS)
+    protected override void ActionResult()
+    {
+        working = true;
+        Debug.Log($"Hello Action {count}");
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= Interval)
+        {
+            time -= Interval;
+            Debug.Log($"Goodbye Action {count++}");
+            working = false;
+            OnActionEnd();
+        }
+    }
+}
