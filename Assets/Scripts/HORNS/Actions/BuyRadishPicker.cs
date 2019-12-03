028c20ca1cf54e1bc23e71d44be34964c4bf75f9
vrooo <mizuram@student.mini.pw.edu.pl>
2019-12-04 00:40:46 +0100

    Begin implementing Seller Agent

---
+++ Assets/Scripts/HORNS/Actions/BuyRadishPicker.cs
@@ -0,0 +1,50 @@
+﻿using HORNS;
+using UnityEngine;
+
+public class BuyRadishPicker : BasicAction
+{
+    private Navigator navigator;
+
+    public BoolVariable SellerResting;
+    public IntVariable RadishPickerDurability;
+    public IntVariable SellerMoney;
+    public Transform Seller;
+
+    protected override void Perform()
+    {
+        navigator.GoTo(Seller, OnActionEnd);
+    }
+
+    protected override void OnActionEnd(bool success)
+    {
+        base.OnActionEnd(success);
+        if (success)
+        {
+
+        }
+        else
+        {
+            navigator.Stop();
+        }
+    }
+
+    protected override void SetupAction(Action action)
+    {
+        base.SetupAction(action);
+        action.AddPrecondition(SellerResting.Variable, new BooleanPrecondition(false));
+        action.AddResult(RadishPickerDurability.Variable, new IntegerAddResult(5));
+        action.AddResult(SellerMoney.Variable, new IntegerAddResult(3));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navigator = GetComponent<Navigator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/HORNS/Reactors/MoneyReactor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyReactor : VariableReactor<int>
+{
+    public IntVariable Money;
+
+    protected override bool ShouldRecalculate(int value)
+    {
+        return Money.Variable.Value > 10;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        Money.LibVariable.Observe(this);
+    }
+}
