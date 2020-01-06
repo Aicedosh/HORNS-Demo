8966e15eca5233cb26efa7d2cc1af757b76d3b2b
vrooo <mizuram@student.mini.pw.edu.pl>
2020-01-06 22:55:56 +0100

    Add camera path

---
+++ Assets/Scripts/UI/CameraSwitchController.cs
@@ -0,0 +1,32 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(CinemachineBrain))]
+public class CameraSwitchController : MonoBehaviour
+{
+    CinemachineBrain brain;
+    public PlayableDirector DollyDirector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        brain = GetComponent<CinemachineBrain>();
+        DollyDirector.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            brain.enabled = true;
+            DollyDirector.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            brain.enabled = false;
+            DollyDirector.Stop();
+        }
+    }
+}
