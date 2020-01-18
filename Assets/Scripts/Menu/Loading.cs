62abf45ba076d0fb35420f6b5061b49c31b90487
Andrzej Lamecki <andrzej4172.lama@gmail.com>
2020-01-18 01:34:52 +0100

    Build fixes

--- Assets/Scripts/Menu/Loading.cs
+++ Assets/Scripts/Menu/Loading.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SceneManagement;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour
