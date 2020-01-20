c3d2a7e1ff4fbc21be1021bccf19236a555ab161
Andrzej Lamecki <andrzej4172.lama@gmail.com>
2020-01-20 22:39:09 +0100

    Add command line options for performance tests

---
+++ Assets/Scripts/Agents/TimeLogger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+public class TimeLogger
+{
+    private const string FOLDER = "PlanTime";
+    StreamWriter sw;
+
+    public TimeLogger(string filename)
+    {
+        var di = Directory.CreateDirectory(FOLDER);
+        sw = new StreamWriter(Path.Combine(FOLDER, filename));
+    }
+
+    public void LogPlanTime(double time)
+    {
+        sw.WriteLine(time.ToString(CultureInfo.InvariantCulture));
+        sw.Flush();
+    }
+
+    public void Destroy()
+    {
+        sw.Close();
+    }
+}
---
+++ Assets/Scripts/CommandLineParser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandLineParser : MonoBehaviour
+{
+    public static bool SkipMenu;
+    public static bool DisableCollisions = true;
+    public static bool LogTimes;
+    public static int? QuitAfter;
+    public static int? Merchants;
+    public static int? Woodcutters;
+    public static int? Carpenters;
+    public static int? Farmers;
+
+    private void Start()
+    {
+        string[] args = Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            string item = args[i];
+            if (item.StartsWith("--"))
+            {
+                string arg = item.Substring(2);
+
+                if(i < args.Length - 1)
+                {
+                    if (arg == "quit-after")
+                    {
+                        QuitAfter = int.Parse(args[i + 1]);
+                    }
+
+                    if (arg == "merchants")
+                    {
+                        Merchants = int.Parse(args[i + 1]);
+                    }
+                    if (arg == "woodcutters")
+                    {
+                        Woodcutters = int.Parse(args[i + 1]);
+                    }
+                    if (arg == "carpenters")
+                    {
+                        Carpenters = int.Parse(args[i + 1]);
+                    }
+                    if (arg == "farmers")
+                    {
+                        Farmers = int.Parse(args[i + 1]);
+                    }
+                }
+
+                if(arg == "skip-menu")
+                {
+                    SkipMenu = true;
+                }
+
+                if (arg == "disable-collisions")
+                {
+                    DisableCollisions = true;
+                }
+
+                if (arg == "log-times")
+                {
+                    LogTimes = true;
+                }
+            }
+        }
+    }
+}
